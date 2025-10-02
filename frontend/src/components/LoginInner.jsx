d12f6c4eb5b826e29b7245981e06658a816bf8e6
Manish Kumar Kasaudhan <77655297+Manishgupta47@users.noreply.github.com>
2025-10-02 15:34:09 +0530

    Update LoginInner.jsx

--- frontend/src/components/LoginInner.jsx
+++ frontend/src/components/LoginInner.jsx
@@ -22,7 +22,7 @@ const LoginInner = () => {
     setError("");
 
     try {
-      const res = await axios.post("http://localhost:5000/login", {
+      const res = await axios.post("https://x-dyat-gaming.onrender.com/login", {
         email,
         password,
       });
